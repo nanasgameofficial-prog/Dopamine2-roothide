6688096e0f0c9ec57820dfad8c98afe5d0a1ba94
nanasgameofficial-prog <nanasgame.official@gmail.com>
2026-01-20 20:10:55 +0700

    Update DOEnvironmentManager.h

--- Application/Dopamine/Jailbreak/DOEnvironmentManager.h
+++ Application/Dopamine/Jailbreak/DOEnvironmentManager.h
@@ -41,7 +41,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (void)runAsRoot:(void (^)(void))rootBlock;
 
 - (void)respring;
-- (void)rebootUserspace;
+- (void)semiReboot;
 - (void)refreshJailbreakApps;
 - (void)reboot;
 - (void)changeMobilePassword:(NSString *)newPassword;
